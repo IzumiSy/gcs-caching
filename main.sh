4c49357663d8c41cd710604cff8c02cfa2cc0c58
IzumiSy <beetle-noise@gmx.com>
2021-10-20 16:48:14 +0900

    fix

--- main.sh
+++ main.sh
@@ -4,18 +4,18 @@ op=$1
 local_=$2
 remote=$3
 cache_key=$4
-archive=$(`md5sum $cache_key`).tar.gz
+archive=$(md5sum $cache_key).tar.gz
 
 case $op in
   "restore")
-    gsutil -q cp $remote/$archive /tmp
+    gsutil -q cp -r $remote/$archive /tmp
     tar -xzf /tmp/$archive -C $local_
     rm /tmp/$archive
     ;;
 
   "store")
     tar -czf /tmp/$archive $local_
-    gsutil -q cp /tmp/$archive $remote
+    gsutil -q cp -r /tmp/$archive $remote
     rm /tmp/$archive
     ;;
 
